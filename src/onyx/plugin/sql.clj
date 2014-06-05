cc1b8fa547225ed57f2129f0ede150863f296702
Michael Drogalis <mjd3089@rit.edu>
2014-06-05 14:33:39 -0400

    API ns

--- src/onyx/plugin/sql.clj
+++ src/onyx/plugin/sql.clj
@@ -24,18 +24,18 @@
 
 (defmethod l-ext/inject-lifecycle-resources
   :sql/load-rows
-  [_ {:keys [task-map] :as pipeline}]
-  {:params [(task->pool task-map)]})
+  [_ {:keys [onyx.core/task-map] :as pipeline}]
+  {:onyx.core/params [(task->pool task-map)]})
 
 (defmethod l-ext/apply-fn [:input :sql]
-  [{:keys [task-map] :as pipeline}]
+  [{:keys [onyx.core/task-map] :as pipeline}]
   (let [pool (task->pool task-map)
         sql-map {:select [:%count.*] :from [(:sql/table task-map)]}
         n ((keyword "count(*)") (first (jdbc/query pool (sql/format sql-map))))
         ranges (partition-all 2 1 (range (or (:sql/lower-bound task-map) 1)
                                          (or (:sql/upper-bound task-map) n)
                                          (:sql/rows-per-segment task-map)))]
-    {:results
+    {:onyx.core/results
      (map (fn [[l h]]
             {:low l
              :high (dec (or h (inc (or (:sql/upper-bound task-map) n))))
