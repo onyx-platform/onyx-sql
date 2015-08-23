42ed0efc809e3c4ca70cdee16455dbebe4203fc9
Michael Drogalis <mjd3089@rit.edu>
2015-08-22 19:21:59 -0700

    Voom onyx-sql.

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject org.onyxplatform/onyx-sql "0.7.1-SNAPSHOT"
+(defproject org.onyxplatform/onyx-sql "0.7.3-SNAPSHOT"
   :description "Onyx plugin for JDBC-backed SQL databases"
   :url "https://github.com/MichaelDrogalis/onyx-sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.3"]
+                 ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
+                 [org.onyxplatform/onyx "0.7.3-20150821_215046-g67feccf"]
                  [java-jdbc/dsl "0.1.3"]
                  [com.mchange/c3p0 "0.9.2.1"]
-                 [org.onyxplatform/onyx "0.7.0"]
                  [honeysql "0.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]
                                   [environ "1.0.0"]
