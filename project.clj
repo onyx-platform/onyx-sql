feca367a75f626ee15dd9c5e2e94000217e2af9e
Michael Drogalis <mjd3089@rit.edu>
2015-09-02 12:29:19 -0400

    Release target.

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"snapshots" {:url "https://clojars.org/repo"
                               :username :env
-                              :password :env}}
+                              :password :env}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.3"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
