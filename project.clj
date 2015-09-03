88002b00e463060243d20e215585dcbe2b883a92
OnyxBot <michael.drogalis@onyxplatform.org>
2015-09-03 15:03:36 +0000

    Release version 0.7.3-beta8.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-sql "0.7.3-SNAPSHOT"
+(defproject org.onyxplatform/onyx-sql "0.7.3-beta8"
   :description "Onyx plugin for JDBC-backed SQL databases"
   :url "https://github.com/MichaelDrogalis/onyx-sql"
   :license {:name "Eclipse Public License"
