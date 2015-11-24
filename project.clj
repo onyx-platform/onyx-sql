7e6d4f7f93336651b8985f4bb1af890a9fc9f49c
OnyxBot <michael.drogalis@onyxplatform.org>
2015-11-24 17:39:26 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-sql "0.8.1.0-alpha3"
+(defproject org.onyxplatform/onyx-sql "0.8.1.0-SNAPSHOT"
   :description "Onyx plugin for JDBC-backed SQL databases"
   :url "https://github.com/MichaelDrogalis/onyx-sql"
   :license {:name "Eclipse Public License"
