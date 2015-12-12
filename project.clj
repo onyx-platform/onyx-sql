e78e8fa226b52eb9c45591c34872bb0c3a12a2b9
OnyxBot <michael.drogalis@onyxplatform.org>
2015-12-12 01:57:43 +0000

    Update dependency org.onyxplatform/onyx to version 0.8.3-20151212_014953-g36da75c.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.3"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.8.3-20151211_164939-g9a500cf"]
+                 [org.onyxplatform/onyx "0.8.3-20151212_014953-g36da75c"]
                  [java-jdbc/dsl "0.1.3"]
                  [com.mchange/c3p0 "0.9.2.1"]
                  [honeysql "0.5.1"]]
