2673baa9717b70cefc75804c2187115c382700d8
Gardner Vickers <gardner@vickers.me>
2016-08-01 22:30:09 -0400

    remove config.edn from uberjar

--- project.clj
+++ project.clj
@@ -22,5 +22,6 @@
   :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.25"]]
                    :plugins [[lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
-                             [lein-pprint "1.1.1"]]}
+                             [lein-pprint "1.1.1"]]
+                   :resource-paths ["test-resources/"]}
              :circle-ci {:jvm-opts ["-Xmx4g"]}})
