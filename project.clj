09b20c2aa922a1a141ea37ca4d38e209d737ec90
Leon Mergen <leon@solatis.com>
2017-05-19 13:35:06 +0200

    Upgrade to onyx 0.10.0-beta14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-sql "0.9.15.0"
+(defproject org.onyxplatform/onyx-sql "0.10.0-beta14"
   :description "Onyx plugin for JDBC-backed SQL databases"
   :url "https://github.com/onyx-platform/onyx-sql"
   :license {:name "Eclipse Public License"
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.3"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.9.15"]
+                 [org.onyxplatform/onyx "0.10.0-beta14"]
                  [java-jdbc/dsl "0.1.3"]
                  [com.mchange/c3p0 "0.9.2.1"]
                  [aero "0.2.0"]
