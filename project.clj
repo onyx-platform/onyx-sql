0d999bd1999d395d1e43bc9519731707d32ff2a7
OnyxBot <michael.drogalis@onyxplatform.org>
2017-11-06 02:50:01 +0000

    Release version 0.12.0.0-alpha4.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-sql "0.12.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-sql "0.12.0.0-alpha4"
   :description "Onyx plugin for JDBC-backed SQL databases"
   :url "https://github.com/onyx-platform/onyx-sql"
   :license {:name "Eclipse Public License"
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.7.0-alpha3"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.12.0-alpha2"]
+                 [org.onyxplatform/onyx "0.12.0-alpha4"]
                  [java-jdbc/dsl "0.1.3"]
                  [com.mchange/c3p0 "0.9.5.2"]
                  [aero "0.2.0"]
